7d421de72082454e6201169e09d09a631d0e6cb4
cielak <ci3l4k@gmail.com>
2021-12-11 22:39:00 +0100

    external call file added

---
+++ main.py
@@ -0,0 +1,11 @@
+import modules
+from time import sleep
+
+if __name__ == '__main__':
+    a = modules.paramiko_ssh_client("192.168.1.104", "root", "odroid")
+    a.run_command("df -h")
+    a.run_command("df -az")
+    a.send_file("test.txt", "/root/test.txt")
+    a.get_file("/root/test.txt", "test1.txt")
+    sleep(2)
+    a.close_connection()
