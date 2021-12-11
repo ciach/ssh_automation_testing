269baa0efb8aa42f3bcaaf8da216370f9961a99c
cielak <ci3l4k@gmail.com>
2021-12-11 22:36:33 +0100

    updated colors and functionalities

--- modules.py
+++ modules.py
@@ -1,33 +1,32 @@
 import logging
+import os
 import paramiko
 import socket
+# import sys
 from datetime import datetime
-from os import path
 from rich.console import Console
+from rich.theme import Theme
 from rich.traceback import install
 from rich import print
-from sys import stdout
-from time import sleep
-from rich.progress import track
+# from time import sleep
+# from rich.progress import track
+
+custom_theme = Theme({
+    "info": "dim white",
+    "info2": "bold blue_violet",
+    "warning": "magenta",
+    "danger": "bold red"
+})
 
 install()
-console = Console(record=True)
+console = Console(record=True, theme=custom_theme)
 
-# ["192.168.1.104", "192.168.1.106", "192.168.1.102", "192.168.1.108", "192.168.1.110",]
-
-log_dir = ""
+logger_file = datetime.now().strftime("%Y-%m-%d_%H-%M-%S") + "_script.log"
 
 msg_format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
 
-logging.basicConfig(filename='example.log', format=msg_format, level="INFO")
-root = logging.getLogger('ssh-client')
-root.setLevel(logging.INFO)
-
-# handler = logging.StreamHandler(stdout)
-# handler.setLevel(logging.DEBUG)
-# formatter = logging.Formatter(msg_format)
-# handler.setFormatter(formatter)
-# root.addHandler(handler)
+logging.basicConfig(filename=logger_file, format=msg_format, level="INFO")
+logger = logging.getLogger('ssh-client')
 
 
 def check_port(host, port):
@@ -41,6 +40,10 @@ def check_port(host, port):
         print(f"Port {port} at {host} is [bold red]closed[/bold red].")
         a_socket.close()
         return False
+
+
+def get_now_time():
+    return str(datetime.now().strftime("%Y/%m/%d %H:%M:%S"))
 
 
 class paramiko_ssh_client:
@@ -63,7 +66,7 @@ class paramiko_ssh_client:
         self.client.set_missing_host_key_policy(
             paramiko.AutoAddPolicy())  # Establish default policy to get locally the host key
         try:
-            root.info("Connecting to: %s" % self.ip)
+            logger.info("Connecting to: %s" % self.ip)
             self.client.connect(
                 hostname=self.ip,
                 port=self.port,
@@ -71,23 +74,29 @@ class paramiko_ssh_client:
                 password=self.passwd,
                 timeout=self.timeout,
                 look_for_keys=False)
-            root.info("Connected to %s" % self.ip)
-            print(f"[green]Connected to: {self.ip}[/green]")
+            logger.info("Connected to %s" % self.ip)
+            time_now = get_now_time()
+            console.print(f"[info]{time_now}[/info] [green]{self.ip}[/green]: Connection established!")
         except paramiko.ssh_exception.NoValidConnectionsError as e:
-            root.exception("Can't connect to: '%s': '%s'", self.ip, e)
+            logger.exception("Can't connect to: '%s': '%s'", self.ip, e)
             console.print_exception()
-
 
     def run_command(self, command):
         """ Run command into the Host
 
         :param command: str which is going to write in the console of the Host
         """
-        print("running command:", command)
+        logger.info("Running command: %s @ %s" % (command, self.ip))
+        time_now = get_now_time()
+        console.print(f"[info]{time_now}[/info] {self.ip}: Running command: [bold]{command}[/bold]")
         _stdin, _stdout, _stderr = self.client.exec_command(command)
         _stdout.channel.recv_exit_status()  # wait for finishing command
         stderr = _stderr.read().decode("utf8").strip()
         stdout = _stdout.read().decode("utf8").strip()
+        if len(stdout) > 0:
+            logger.info("Command: %s output from %s:\n %s" % (command, self.ip, stdout))
+        if len(stderr) != 0:
+            logger.error("Command: %s error from %s:\n %s" % (command, self.ip, stderr))
         return stderr, stdout
 
     def send_file(self, from_path, to_path):
@@ -96,26 +105,15 @@ class paramiko_ssh_client:
         :param from_path: str Variable which specifies the path where the file is in this pc
         :param to_path: str variable which specifies the path where the file is going to be send
         """
-        print("sending file: from", from_path, "to", to_path)
+        time_now = get_now_time()
+        console.print(
+            f"[info]{time_now}[/info] {self.ip}: Sending file from [info2]{from_path}[/info2] to [bold]{to_path}[/bold]")
         t = paramiko.Transport((self.ip, 22))
         t.connect(username=self.user, password=self.passwd)
         sftp = paramiko.SFTPClient.from_transport(t)
         # sftp = self.client.open_sftp()
         sftp.put(from_path, to_path)
-        return str('File sent from path -> {} to path -> {}'.format(from_path, to_path))
-
-    def get_file(self, from_path, to_path):
-        """ Download file to a host using SFTP protocol, which is included in SSH
-
-        :param from_path: str Variable which specifies the path where the file is in this pc
-        :param to_path: str variable which specifies the path where the file is going to be send
-        """
-        print("sending file: from", from_path, "to", to_path)
-        t = paramiko.Transport((self.ip, 22))
-        t.connect(username=self.user, password=self.passwd)
-        sftp = paramiko.SFTPClient.from_transport(t)
-        # sftp = self.client.open_sftp()
-        sftp.get(from_path, to_path)
+        logger.info('File sent from path -> %s to path -> %s with %s' % (from_path, to_path, self.ip))
         return str('File sent from path -> {} to path -> {}'.format(from_path, to_path))
 
     def send_all_folder_files(self, origin_folder, destination_folder):
@@ -125,12 +123,31 @@ class paramiko_ssh_client:
         :param destination_folder: str variable which specifies the path where the files are going to be send
         """
         for file in os.listdir(origin_folder):
-            self.send_file(path.join(origin_folder, file), path.join(destination_folder, file))
+            self.send_file(os.path.join(origin_folder, file), os.path.join(destination_folder, file))
+
+    def get_file(self, from_path, to_path):
+        """ Download file to a host using SFTP protocol, which is included in SSH
+
+        :param from_path: str Variable which specifies the path where the file is in this pc
+        :param to_path: str variable which specifies the path where the file is going to be send
+        """
+        time_now = get_now_time()
+        console.print(
+            f"[info]{time_now}[/info] {self.ip}: Getting file from [bold]{from_path}[/bold] to [info2]{to_path}[/info2]")
+        t = paramiko.Transport((self.ip, 22))
+        t.connect(username=self.user, password=self.passwd)
+        sftp = paramiko.SFTPClient.from_transport(t)
+        # sftp = self.client.open_sftp()
+        sftp.get(from_path, to_path)
+        logger.info('File get from path -> %s to path -> %s with %s' % (from_path, to_path, self.ip))
+        return str('File sent from path -> {} to path -> {}'.format(from_path, to_path))
 
     def close_connection(self):
         """ Close the SSH session with the Host"""
         self.client.close()
-        root.info("Disconnecting from %s" % self.ip)
+        logger.info("Disconnecting from %s" % self.ip)
+        time_now = get_now_time()
+        console.print(f"[info]{time_now}[/info] [green]{self.ip}[/green]: Disconnected")
         return str(f'SSH Connection closed to {self.ip}')
 
     def check_connection(self):
@@ -147,6 +164,7 @@ class paramiko_ssh_client:
             return False
 
 
+"""
 def active_wait_for_dut(dut_ip, max_attempts=100, user='root', passwd='root', connection_timeout=2):
     n_attempts = 1
     while n_attempts <= max_attempts:
@@ -155,7 +173,7 @@ def active_wait_for_dut(dut_ip, max_attempts=100, user='root', passwd='root', co
         except Exception:
             print("Attempting to connect:", n_attempts, "out of", max_attempts, "...")
             n_attempts += 1
-            time.sleep(5)
+            sleep(5)
             continue
         else:
             print("DUT is ON")
@@ -163,16 +181,5 @@ def active_wait_for_dut(dut_ip, max_attempts=100, user='root', passwd='root', co
             return
     # print("DUT is NOT connected.")
     raise Exception("DUT is NOT connected.")
+"""
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    a = paramiko_ssh_client("192.168.1.104", "root", "odroid")
-    sleep(5)
-    a.close_connection()
-    sleep(1)
-    a = paramiko_ssh_client("192.168.1.102", "root", "odroid")
-    sleep(5)
-    a.close_connection()
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
