f4f9e2a9058d490f7390fb63e9178b30f3e0f139
cielak <ci3l4k@gmail.com>
2021-12-15 23:34:57 +0100

    changes for ssh2 python test

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 import modules
 from time import sleep
 
+
+
+
+
+
+
 if __name__ == '__main__':
-    a = modules.paramiko_ssh_client("192.168.1.104", "root", "odroid")
+    a = modules.ssh2_ssh_client("192.168.1.104", "root", "odroid")
     a.run_command("df -h")
     a.run_command("df -az")
     a.send_file("test.txt", "/root/test.txt")
     a.get_file("/root/test.txt", "test1.txt")
-    sleep(2)
-    a.close_connection()
+    # a = modules.paramiko_ssh_client("192.168.1.106", "root", "odroid")
+    # a.run_command("df -h")
+    # a.run_command("df -az")
+    # a.send_file("test.txt", "/root/test.txt")
+    # a.get_file("/root/test.txt", "test1.txt")
+    # sleep(2)
+    # a.close_connection()
+
